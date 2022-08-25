7da0c61ca62b818493d0cd300cad32fa98f2c73b
JOchoa24 <ju_andre.1224@hotmail.com>
2022-08-24 19:53:45 -0500

    creacion proyecto

---
+++ sistemadegestion/src/main/java/com/datasoft/sistemadegestion/SistemadegestionApplication.java
@@ -0,0 +1,13 @@
+package com.datasoft.sistemadegestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadegestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadegestionApplication.class, args);
+	}
+
+}
---
+++ sistemadegestion/src/test/java/com/datasoft/sistemadegestion/SistemadegestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datasoft.sistemadegestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadegestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

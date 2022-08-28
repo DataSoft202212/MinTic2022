79ab121d23e28af9b6cde37990caba7631361813
edherney <edherney@gmail.com>
2022-08-28 16:55:47 -0500

    creacion objeto clase

--- sistemadegestion/src/main/java/com/datasoft/sistemadegestion/SistemadegestionApplication.java
+++ sistemadegestion/src/main/java/com/datasoft/sistemadegestion/SistemadegestionApplication.java
@@ -1,5 +1,6 @@
 package com.datasoft.sistemadegestion;
 
+import com.datasoft.sistemadegestion.entidades.MovimientoDinero;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SistemadegestionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SistemadegestionApplication.class, args);
+		MovimientoDinero mov1 = new MovimientoDinero(00001,"2022/02/12","pago prestamo","eder");
+		System.out.println(mov1.toString());
+		System.out.println(mov1.getConceptoMovimientoDinero());
+
+
 	}
 
 }
--- sistemadegestion/src/main/java/com/datasoft/sistemadegestion/entidades/MovimientoDinero.java
+++ sistemadegestion/src/main/java/com/datasoft/sistemadegestion/entidades/MovimientoDinero.java
@@ -1,15 +1,58 @@
 package com.datasoft.sistemadegestion.entidades;
+
+
 public class MovimientoDinero {
     private int numMovimientoDinero;
-    private date fechaMovimientoDinero;
+    private String fechaMovimientoDinero;
     private String conceptoMovimientoDinero;
     private String usuarioMovimientoDinero;
 
-    public void crearMovimiento(){
-
+    public MovimientoDinero(int numMovimientoDinero, String fechaMovimientoDinero, String conceptoMovimientoDinero, String usuarioMovimientoDinero) {
+        this.numMovimientoDinero = numMovimientoDinero;
+        this.fechaMovimientoDinero = fechaMovimientoDinero;
+        this.conceptoMovimientoDinero = conceptoMovimientoDinero;
+        this.usuarioMovimientoDinero = usuarioMovimientoDinero;
     }
 
-    public void modificarMovimiento(){
+    public int getNumMovimientoDinero() {
+        return numMovimientoDinero;
+    }
 
+    public void setNumMovimientoDinero(int numMovimientoDinero) {
+        this.numMovimientoDinero = numMovimientoDinero;
+    }
+
+    public String getFechaMovimientoDinero() {
+        return fechaMovimientoDinero;
+    }
+
+    public void setFechaMovimientoDinero(String fechaMovimientoDinero) {
+        this.fechaMovimientoDinero = fechaMovimientoDinero;
+    }
+
+    public String getConceptoMovimientoDinero() {
+        return conceptoMovimientoDinero;
+    }
+
+    public void setConceptoMovimientoDinero(String conceptoMovimientoDinero) {
+        this.conceptoMovimientoDinero = conceptoMovimientoDinero;
+    }
+
+    public String getUsuarioMovimientoDinero() {
+        return usuarioMovimientoDinero;
+    }
+
+    public void setUsuarioMovimientoDinero(String usuarioMovimientoDinero) {
+        this.usuarioMovimientoDinero = usuarioMovimientoDinero;
+    }
+
+    @Override
+    public String toString() {
+        return "MovimientoDinero{" +
+                "numMovimientoDinero=" + numMovimientoDinero +
+                ", fechaMovimientoDinero='" + fechaMovimientoDinero + '\'' +
+                ", conceptoMovimientoDinero='" + conceptoMovimientoDinero + '\'' +
+                ", usuarioMovimientoDinero='" + usuarioMovimientoDinero + '\'' +
+                '}';
     }
 }

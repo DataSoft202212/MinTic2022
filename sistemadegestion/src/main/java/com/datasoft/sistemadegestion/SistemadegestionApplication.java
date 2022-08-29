9183c9aac12cfe2546fcb5c9e87f87c9654a06f0
JOchoa24 <ju_andre.1224@hotmail.com>
2022-08-28 19:34:26 -0500

    Instancia clase Empresa

--- sistemadegestion/src/main/java/com/datasoft/sistemadegestion/SistemadegestionApplication.java
+++ sistemadegestion/src/main/java/com/datasoft/sistemadegestion/SistemadegestionApplication.java
@@ -1,6 +1,7 @@
 package com.datasoft.sistemadegestion;
 
 import com.datasoft.sistemadegestion.entidades.Empleado;
+import com.datasoft.sistemadegestion.entidades.Empresa;
 import com.datasoft.sistemadegestion.entidades.MovimientoDinero;
 import com.datasoft.sistemadegestion.entidades.Rol;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +16,8 @@ public class SistemadegestionApplication {
 		System.out.println(mov1.toString());
 		Empleado empleado1 = new Empleado("carlos","perez","neiva","987654","carlos@gmail",rol1,mov1);
 		System.out.println(empleado1.toString());
+		Empresa empresa1 = new Empresa(900465357,"DataSoft","Calle 1 No. 1 - 1",1234567,"datasoft@gmail.com");
+		System.out.println(empresa1.toString());
 
 
 

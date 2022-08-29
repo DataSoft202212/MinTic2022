78e235d79336db6f2edb7829e3af03364ee15ae9
edherney <edherney@gmail.com>
2022-08-28 21:42:51 -0500

    Sprint2 final

--- sistemadegestion/src/main/java/com/datasoft/sistemadegestion/SistemadegestionApplication.java
+++ sistemadegestion/src/main/java/com/datasoft/sistemadegestion/SistemadegestionApplication.java
@@ -7,19 +7,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SistemadegestionApplication {
 
 	public static void main(String[] args) {
+		//Instancia de la clase Empresa
 		Empresa empresa1 = new Empresa(900465357,"DataSoft","Calle 1 No. 1 - 1",1234567,"datasoft@gmail.com");
 		System.out.println(empresa1.toString());
+		//update atributo de la clase Empresa
+		empresa1.setDireccionEmpresa("cartagena");
+		System.out.println(empresa1.toString());
+
+		//Instancia de la clase Rol
 		Rol rol1 = new Rol(01,"admin",true);
 		System.out.println(rol1.toString());
+		//Instancia de la clase TipoDocumento
 		TipoDocumento tipoDocumento = new TipoDocumento(001,"Cedula de Ciudadania","CC",true);
 		System.out.println(tipoDocumento.toString());
+
+		//Instancia de la clase Empleado
 		Empleado empleado1 = new Empleado(001,1076543123,tipoDocumento,"oscar","garcia","bogota","8762345","oscgar@gmail.com",rol1,empresa1);
 		System.out.println(empleado1.toString());
+		//update atributo de la clase Empleado
+		empleado1.setApellidoEmpleado("Perez");
+		System.out.println(empleado1.toString());
+
+		//Instancia de la clase MovimientoDinero
 		MovimientoDinero movDinero1 = new MovimientoDinero(00001,"12/05/2022","pago de tarjeta",-3450000,empleado1);
 		System.out.println(movDinero1.toString());
 
-
-
+		//update atributo de la clase MovimientoDinero
+		movDinero1.setValorMovimientoDinero(-56000);
+		System.out.println(movDinero1.getNumMovimientoDinero());
+		System.out.println(movDinero1.toString());
 	}
 
 }

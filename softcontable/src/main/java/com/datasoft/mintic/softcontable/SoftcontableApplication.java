9abc64c4e53806a168f995eb5fa4a07032370dfe
edherney <edherney@gmail.com>
2022-09-06 22:19:27 -0500

    proyecto - thymeleaf

---
+++ softcontable/src/main/java/com/datasoft/mintic/softcontable/SoftcontableApplication.java
@@ -0,0 +1,13 @@
+package com.datasoft.mintic.softcontable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftcontableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftcontableApplication.class, args);
+	}
+
+}
---
+++ softcontable/src/test/java/com/datasoft/mintic/softcontable/SoftcontableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datasoft.mintic.softcontable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftcontableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
